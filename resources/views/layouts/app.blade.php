4aafb3771735bcba586dc079033a98ea49f1e4a4
JeffersonDomingos <dominguesmorais@hotmail.com>
2024-06-01 12:37:14 -0300

    Commit: Start-Front

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,20 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
+    <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
-    <!-- Additional CSS Files -->
-    
-    <link rel="stylesheet" href="{{ asset('resources/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('resources/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('resources/css/owl.carousel.css') }}">
-    <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('resources/css/ecommerce/responsive.css') }}">
 </head>
 <body>
     <div id="app">
@@ -38,16 +29,21 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto"></ul>
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Admin Login') }}</a>
                                 </li>
                             @endif
+
+                            
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -60,6 +56,7 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
@@ -75,15 +72,5 @@
             @yield('content')
         </main>
     </div>
-
-    <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script src="{{ asset('resources/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('resources/js/jquery.sticky.js') }}"></script>
-    <script src="{{ asset('resources/js/jquery.easing.1.3.min.js') }}"></script>
-    <script src="{{ asset('resources/js/main.js') }}"></script>
-    <script src="{{ asset('resources/js/bxslider.min.js') }}"></script>
-    <script src="{{ asset('resources/js/script.slider.js') }}"></script>
 </body>
 </html>
