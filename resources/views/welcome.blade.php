eccb6e8945938b598b6e577f71ba16f737687a09
JeffersonDomingos <dominguesmorais@hotmail.com>
2024-06-01 11:10:21 -0300

    Commit 01-06

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <h1>Bem-vindo ao AdminLTE integrado com Laravel!</h1>
-                </div>
-            </div>
-        </div>
-    </div>
+
+@include('header')
+@include('index')
+@include('footer')
+
 @endsection
